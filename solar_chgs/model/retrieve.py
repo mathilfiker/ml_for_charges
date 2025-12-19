4a12da5a821b1aed98c61f8528993c83b51b1833
mathilfiker <mathias.hilfiker@icloud.com>
2025-12-19 13:40:28 +0100

    Add files via upload

---
+++ solar_chgs/model/retrieve.py
@@ -0,0 +1,11 @@
+import optuna
+import sys
+DB_FILE = 'sqlite:///../optuna_hyperparameter_optimization.db'
+study = optuna.load_study(study_name='xgb_optimization', storage = DB_FILE)
+best_trial = study.best_trial
+print("Best hyperparameters:")
+for key, value in best_trial.params.items():
+    print(f"{key}: {value}")
+
+print(f"Best RMSE: {best_trial.value}")
+sys.stdout.flush()
